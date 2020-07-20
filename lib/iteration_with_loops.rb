e180229dab91de53de5dc165992b8a8923c611a0
Jarrell Jackson <jdjack3@gmail.com>
2020-07-20 00:53:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def join_nested_strings(src)
     inner_index = 0
     while inner_index < src[row_index].count do
       if src[row_index][inner_index].class == String
-      nested_string << scr[row_index][inner_index].join(" ")
+      nested_string << scr[row_index][inner_index]
     end
       inner_index += 1
     end
